05bb747efc765b61fa64c62a3fab31ba5da6a1b3
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-17 17:09:34 +0700

    Create The 2014 ACM-ICPC Asia Jakarta Regional Contest.java

---
+++ The 2014 ACM-ICPC Asia Jakarta Regional Contest.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt(), k = sc.nextInt();
+        int []a = new int[n];
+        for(int i = 0; i < n;i++){
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        int cnt = 1;
+        for(int i = 1;i < n ;i++){
+            if(a[i] - a[i - 1] <= k){
+                continue;
+            }
+            else cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
