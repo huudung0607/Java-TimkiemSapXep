3b4cb60ee2567b8f844f427bd0153c3e3128e763
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-22 09:48:10 +0700

    Create Bersu ball.java

---
+++ Bersu ball.java
@@ -0,0 +1,30 @@
+import com.sun.source.tree.Tree;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt(), m = sc.nextInt();
+        int []a = new int[n]; int []b = new int[m];
+        for(int i = 0;i < n;i++) a[i] = sc.nextInt();
+        for(int i = 0;i < m;i++) b[i] = sc.nextInt();
+        int i = 0, j = 0;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int cnt = 0;
+        while(i < n && j < m){
+            if(Math.abs(a[i] - b[j]) <= 1 || Math.abs(b[j] - a[i]) <= 1){
+                i++;j++;
+                cnt++;
+            }
+            else if(a[i] > b[j]){
+                j++;
+            }
+            else i++;
+        }
+        System.out.print(cnt);
+    }
+}
