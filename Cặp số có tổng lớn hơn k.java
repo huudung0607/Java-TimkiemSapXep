b455d930a5b46cf3c34a85ed45f9253ea6f6d2e8
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-22 08:10:45 +0700

    Create Cặp số có tổng lớn hơn k.java

---
+++ Cặp số có tổng lớn hơn k.java
@@ -0,0 +1,48 @@
+import java.lang.reflect.Array;
+    import java.util.*;
+    import java.lang.*;
+    public class Main {
+        private static int mod = 1000000007;
+        public static Scanner sc = new Scanner(System.in);
+        public static int firstPos(int []a ,int l,int r, int x){
+            int res = -1;
+            while(l <= r){
+                int m = (l + r)/2;
+                if(a[m] == x){
+                    res = m;
+                    r = m -1;
+                }
+                else if(a[m] < x) l = m + 1;
+                else r = m - 1;
+            }
+            return res;
+        }
+        public static int lastPos(int []a ,int l,int r, int x){
+            int res = -1;
+            while(l <= r){
+                int m = (l + r)/2;
+                if(a[m] > x){
+                    res = m;
+                    r = m - 1;
+                }
+                else l = m + 1;
+            }
+            return res;
+        }
+        public static void main(String[] args) {
+            int n = sc.nextInt(), k = sc.nextInt();
+            int []a = new int[n];
+            for(int i = 0;i < n ;i++){
+                a[i] = sc.nextInt();
+            }
+            long ans = 0;
+            Arrays.sort(a);
+            for(int i = 0; i < n ;i++){
+                int pos = lastPos(a,i + 1, n - 1,k - a[i]);
+                if(pos != -1){
+                    ans += 1l * n - pos;
+                }
+            }
+            System.out.print(ans);
+        }
+    }
